ca2c0c1fff06f59c2bbb17bc184f0896a6eba047
wqrxsssr <nsigaeva07@gmail.com>
2026-03-13 08:29:14 +0300

    server: add product controller

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import { ProductController } from './controllers/products/product.controller';
+import { ProductService } from './services/products/product.service';
 
 const userRouter = express.Router();
 const productsRouter = express.Router();
@@ -6,6 +8,12 @@ const basketRouter = express.Router();
 const deliveryRouter = express.Router();
 
 export function configureRouter(app: Application) {
+    const productService = new ProductService();
+    const productController = new ProductController(productService);
+    
+    productsRouter.get('/', (req, res) => productController.list(req, res));
+    productsRouter.get('/:id', (req, res) => productController.getOne(req, res));
+
     app.use('/users', userRouter);
     app.use('/products', productsRouter);
     app.use('/basket', basketRouter);
