653b586cf3abe31c6303e693baa01a72ddd91f0a
Павловец Кирилл <voenikot@mail.ru>
2026-03-23 10:46:32 +0300

    server: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,30 @@ const PORT = 3000;
 
 app.use(express.json());
 
+const allowedOrigins = new Set([
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]);
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin && allowedOrigins.has(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Vary', 'Origin');
+    }
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+    if (req.method === 'OPTIONS') {
+        res.status(204).send();
+        return;
+    }
+    next();
+});
+
 configureRouter(app);
 
 app.get('/', (req: Request, res: Response) => {
